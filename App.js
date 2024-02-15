7f9689b71af4ea19d7315fffcf83b7e03448d1b9
Suman Bharati <sumanbharati07@gmail.com>
2024-02-15 23:45:20 +0530

    ReactJS-Inception

--- App.js
+++ App.js
@@ -7,4 +7,18 @@ const heading = React.createElement(
 const root = ReactDOM.createRoot(document.getElementById("root"));
   
 root.render(heading);
+
+// const content = React.createElement("div", {}, 
+//     [React.createElement("div", {}, 
+//         [React.createElement("h1", {}, "I'm h1 tag."),
+//          React.createElement("h2", {}, "I'm h2 tag.")]
+//     ),
+//     React.createElement("div", {}, 
+//     [React.createElement("h1", {}, "I'm h1 tag."),
+//      React.createElement("h2", {}, "I'm h2 tag.")]
+//     )]
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
   
+// root.render(content);
