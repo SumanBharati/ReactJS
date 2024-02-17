1ebe040906a4daea1f8fc31c3d0a294780b7dd84
Suman Bharati <sumanbharati07@gmail.com>
2024-02-17 05:37:28 +0530

    ReactJS-LayingTheFoundation

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" }, //This object takes html attributes
-    "Hello World from App.js"
-);
-  
+// JSX
+const jsxHeading = <h1 id="heading">Hello React using JSX.</h1>
+
+const reactElem = (<span>
+    {jsxHeading}
+    <h1 id="heading">Hello from another React element.</h1>
+</span>)
+
+const TitleComponent = () => (
+    <div>
+        <h1>Hello from Title component.</h1>
+    </div>
+)
+
+const HeadingComponent = () => (
+    <div>
+        <TitleComponent />
+        {reactElem}
+        <h1>Hello from Heading component.</h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
+
+
+
+// CORE REACT WAY OF CREATING ELEMENTS
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading", xyz: "abc" }, //This object takes html attributes
+//     "Hello React using core React"
+// );
   
-root.render(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+// root.render(heading);
 
 // const content = React.createElement("div", {}, 
 //     [React.createElement("div", {}, 
